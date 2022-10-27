31467ce2bdc384870cc3cd618c957402a3a49126
Evgeny Filipenko <Mohax67@mail.ru>
2022-10-27 13:29:58 +0300

    Initial commit

---
+++ src/main/java/org/homeworks/App.java
@@ -0,0 +1,10 @@
+package org.homeworks;
+
+//import org.homeworks.data.EmployersData;
+
+public class App {
+    public static void main(String[] args) {
+/*        EmployersData employersData = new EmployersData();
+        employersData.showInfo();*/
+    }
+}
